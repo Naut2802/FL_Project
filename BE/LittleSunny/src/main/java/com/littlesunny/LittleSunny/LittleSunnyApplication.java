f17db0187e1602d6a1e78f0257c5f4cbd3e61ea6
Naut2802 <willbin012@gmail.com>
2024-06-14 03:56:43 +0700

    Created BE folder

---
+++ BE/LittleSunny/src/main/java/com/littlesunny/LittleSunny/LittleSunnyApplication.java
@@ -0,0 +1,13 @@
+package com.littlesunny.LittleSunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleSunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleSunnyApplication.class, args);
+	}
+
+}
