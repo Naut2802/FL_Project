40787114c9a2224afc49b39d3e9311c4810844e1
Le Minh Hoang <leminhhoang241299@gmail.com>
2024-06-22 01:55:32 +0700

    doi table sang table MUI

--- FE/LittleSunny/src/Components/Layout/DSHocVien/DSHocVien.js
+++ FE/LittleSunny/src/Components/Layout/DSHocVien/DSHocVien.js
@@ -1,52 +1,88 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
 function DSHocVien() {
     return (
-        <div className="container card mt-4 border bg-white rounded-4">
-            <div className="card-header row my-2 text-center">
-                <h1 className="col-10">Danh Sách Học Viên Trung Tâm</h1>
-                <input className="col-2 rounded-5" type="text" placeholder="Tìm kiếm" />
-            </div>
-            <div className="card-body">
-                <table className="table table-striped-columns mt-2">
-                    <thead>
-                        <tr>
-                            <th scope="col">Khóa Học</th>
-                            <th scope="col">Lớp</th>
-                            <th scope="col">Họ Tên</th>
-                            <th scope="col">Ngày Sinh</th>
-                            <th scope="col">Địa Chỉ</th>
-                            <th scope="col">Số ĐT</th>
-                            <th scope="col">TT Phụ Huynh</th>
-                            <th scope="col">Học Phí</th>
-                            <th scope="col">Trạng Thái</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Khóa ...</td>
-                            <td>Lớp ...</td>
-                            <td>Nguyễn Văn A</td>
-                            <td>02/02/2010</td>
-                            <td>abc/edf</td>
-                            <td>xxxx.xxx.xxx</td>
-                            <td>Phụ Huynh A</td>
-                            <td>300.000</td>
-                            <td>Đã Đóng Học Phí</td>
-                        </tr>
-                        <tr>
-                            <td>Khóa ...</td>
-                            <td>Lớp ...</td>
-                            <td>Nguyễn Văn B</td>
-                            <td>02/02/2010</td>
-                            <td>abc/edf</td>
-                            <td>xxxx.xxx.xxx</td>
-                            <td>Phụ Huynh B</td>
-                            <td>300.000</td>
-                            <td>Chưa Đóng Học Phí</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        <TableContainer sx={{ width: 1200, marginTop: 5, marginLeft: 10 }} component={Paper}>
+            <h1 className="mt-3" align="center">
+                DANH SÁCH HỌC VIÊN
+            </h1>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell sx={{ fontSize: 20 }}>Khóa Học</TableCell>
+                        <TableCell sx={{ fontSize: 20 }}>Lớp</TableCell>
+                        <TableCell sx={{ fontSize: 20 }} align="center">
+                            Họ Tên
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 20 }} align="center">
+                            Ngày Sinh
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 20 }} align="center">
+                            Địa Chỉ
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 20 }} align="center">
+                            Số ĐT
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 20 }} align="center">
+                            TT Phụ Huynh
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 20 }} align="center">
+                            Học Phí
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 20 }} align="center">
+                            Trạng Thái
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow
+                        sx={{
+                            '&:last-child td, &:last-child th': { border: 0 },
+                            '&:hover': {
+                                backgroundColor: 'grey',
+                            },
+                        }}
+                    >
+                        <TableCell sx={{ fontSize: 15 }} component="th" scope="row">
+                            Khóa...
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 15 }}>Lớp ...</TableCell>
+                        <TableCell sx={{ fontSize: 15 }} align="center">
+                            Nguyễn Văn A
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 15 }} align="center">
+                            20/02/2010
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 15 }} align="center">
+                            153/153 abc Q.B Tp.abcc
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 15 }} align="center">
+                            xxxx .xxx .xxx
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 15 }} align="center">
+                            Phụ Huynh A
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 15 }} align="center">
+                            4.000.0000
+                        </TableCell>
+                        <TableCell sx={{ fontSize: 15 }} align="center">
+                            Chưa thanh toán
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            <Stack spacing={2} sx={{ float: 'inline-end', margin: 2 }}>
+                <Pagination count={10} />
+            </Stack>
+        </TableContainer>
     );
 }
 
--- FE/LittleSunny/src/Components/Layout/DSLop_KhoaHoc/DSLopKhoaHoc.js
+++ FE/LittleSunny/src/Components/Layout/DSLop_KhoaHoc/DSLopKhoaHoc.js
@@ -1,6 +1,15 @@
-import Table from 'react-bootstrap/Table';
-import styles from './DSLopKhoaHoc.module.scss';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+// import Table from 'react-bootstrap/Table';
 import classNames from 'classnames/bind';
+import styles from './DSLopKhoaHoc.module.scss';
 
 const cx = classNames.bind(styles);
 function DSLopKhoaHoc() {
@@ -11,9 +20,6 @@ function DSLopKhoaHoc() {
                     <form action="">
                         <h1>Thêm Khóa - Lớp Học</h1>
                         <hr />
-                        {/* <div className={cx('input-box')}>
-                            <input type="text" placeholder="Khóa Học" required />
-                        </div> */}
                         <div className={cx('input-box')}>
                             <select className="form-select" defaultValue="0">
                                 <option value="0">Khóa Học</option>
@@ -37,36 +43,44 @@ function DSLopKhoaHoc() {
                     </form>
                 </div>
             </div>
-            <div className="card my-4 border bg-white rounded-4">
-                <div className="card-header row my-2 text-center">
-                    <h1 className="col-10">Danh Sách Lớp</h1>
-                    <select className="col-2 rounded-5" placeholder="Tìm kiếm" defaultValue="0">
-                        <option value="0">Chọn Khóa...</option>
-                        <option value="1">Khóa...</option>
-                        <option value="2">Khóa...</option>
-                    </select>
-                </div>
-                <div className="card-body">
-                    <Table striped bordered hover size="sm">
-                        <thead className="text-center">
-                            <tr>
-                                <th scope="col">Lớp</th>
-                                <th scope="col">Khóa</th>
-                                <th scope="col">Trạng Thái</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td className="w-25">Lớp ...</td>
-                                <td className="w-25">Khóa ...</td>
-                                <td className="w-25 text-center">
-                                    <a href="a">Chi tiết</a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </div>
+
+            <TableContainer sx={{ width: 1200, marginTop: 5, marginLeft: 10 }} component={Paper}>
+                <h1 className="mt-3" align="center">
+                    DANH SÁCH LỚP - KHÓA HỌC
+                </h1>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ fontSize: 20 }}>Khóa Học</TableCell>
+                            <TableCell sx={{ fontSize: 20 }}>Lớp</TableCell>
+                            <TableCell sx={{ fontSize: 20 }} align="center">
+                                Trạng Thái
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow
+                            sx={{
+                                '&:last-child td, &:last-child th': { border: 0 },
+                                '&:hover': {
+                                    backgroundColor: 'grey',
+                                },
+                            }}
+                        >
+                            <TableCell sx={{ fontSize: 15 }} component="th" scope="row">
+                                Khóa...
+                            </TableCell>
+                            <TableCell sx={{ fontSize: 15 }}>Lớp ...</TableCell>
+                            <TableCell sx={{ fontSize: 15 }} align="center">
+                                ...
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+                <Stack spacing={2} sx={{ float: 'inline-end', margin: 2 }}>
+                    <Pagination count={10} />
+                </Stack>
+            </TableContainer>
         </div>
     );
 }
